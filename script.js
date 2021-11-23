b0ded522cc8cdd4caf49720ca1e8ae74a0e5b3f6
danalvrz <dante.alvrz@gmail.com>
2021-11-23 16:55:36 -0600

    Change object to classes

--- script.js
+++ script.js
@@ -1,58 +1,81 @@
-let books = [
-  {
-    title: 'Uno Lorem',
-    author: 'Testero Testy',
-  },
-  {
-    title: 'Dos Lorem',
-    author: 'Testero Testy2',
-  },
-];
-
-function displayBooks() {
-  document.getElementById('book-list').innerHTML = '';
-  for (let i = 0; i < books.length; i += 1) {
-    document.getElementById('book-list').innerHTML += `
-  <li class="book-card">
-    <p>${books[i].title}</p>
-    <p>${books[i].author}</p>
-    <button id="remove${i}"class="remove" onclick="removeBook(this.id)">Remove</button>
-  </li>`;
+class newBook {
+  constructor(title, author, idBook) {
+    this.title = title;
+    this.author = author;
+    this.idBook = idBook;
   }
 }
 
-function catchValue() {
-  const saveCollection = JSON.stringify(books);
-  localStorage.setItem('books', saveCollection);
-}
-/* eslint-disable no-unused-vars */
-function addBook() {
-  const newBook = {};
-  newBook.title = document.getElementById('title').value;
-  newBook.author = document.getElementById('author').value;
-  if (newBook.title !== '' && newBook.author !== '') {
-    books.push(newBook);
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-    catchValue();
-    displayBooks();
+class books {
+  constructor() {
+    this.books = [];
+  }
+  // Store book array
+  saveValue() {
+    const saveCollection = JSON.stringify(books);
+    localStorage.setItem("books", saveCollection);
+  }
+  // Get book array
+  getValue() {
+    if (localStorage.getItem("books")) {
+      this.books = JSON.parse(localStorage.getItem("books"));
+      catchValue();
+    }
+  }
+  // Add book to array
+  addBook(title, author) {
+    const currentBook = new newBook(title, author);
+    currentBook.idBook = this.books.length + 1;
+    if (currentBook.title !== "" && currentBook.author !== "") {
+      this.books.push(currentBook);
+      document.getElementById("title").value = "";
+      document.getElementById("author").value = "";
+      this.displayBooks();
+    }
+  }
+  // Remove book from array
+  removeBook(idBook) {
+    this.books = this.books.filter((id) => id !== idBook);
+    this.displayBooks();
+  }
+  // Display books
+  displayBooks() {
+    document.getElementById("book-list").innerHTML = "";
+    for (let i = 0; i < this.books.length; i += 1) {
+      document.getElementById("book-list").innerHTML += `
+    <li class="book-card">
+      <p>${this.books[i].title}</p>
+      <p>${this.books[i].author}</p>
+      <button id="remove${this.books[i].idBook}"class="remove">Remove</button>
+    </li>`;
+    }
   }
 }
-/* eslint-disable no-unused-vars */
-function removeBook(id) {
-  const bookDeleted = id.replace('remove', '');
-  books.splice(bookDeleted, 1);
-  catchValue();
-  displayBooks();
-}
 
-function getValue() {
-  if (localStorage.getItem('books')) {
-    books = JSON.parse(localStorage.getItem('books'));
-    catchValue();
-  }
-}
+const myBooks = new books();
+const addNewBook = new newBook("title uno", "author uno", "1");
+const addNewBook2 = new newBook("title dos", "author dos", "2");
+myBooks.books.push(addNewBook);
+myBooks.books.push(addNewBook2);
 
-getValue();
-window.onload = getValue;
-displayBooks();
+myBooks.displayBooks();
+
+const removeButtons = document.querySelectorAll(".remove");
+
+document.querySelectorAll(".remove").forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log(myBooks.books);
+  });
+});
+
+const addBtn = document.querySelector("#add");
+addBtn.addEventListener("click", () => {
+  let currentTitle = document.querySelector("#title").value;
+  let currentAuthor = document.querySelector("#author").value;
+  myBooks.addBook(currentTitle, currentAuthor);
+});
+
+let arr = [1, 2, 3, 4,5,6,7];
+const idx = 3;
+
+arr = arr.filter((id) => id !== idx);
