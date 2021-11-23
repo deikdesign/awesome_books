473c55fa6d9c3377549ca394db988c4c8bb34b1a
danalvrz <dante.alvrz@gmail.com>
2021-11-23 10:44:17 -0600

    Fix version error

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
-const titleInput = document.getElementById("title").value;
-const authorInput = document.getElementById("author").value;
-
-class Book {
-    constructor (title, author){
-        this.title = title;
-        this.author = author;
-    }
-};
-
-const books = [
-    { title: 'Uno Lorem',
-      author: 'Testero Testy',
-    },
-    { title: 'Dos Lorem',
-      author: 'Testero Testy2',
-    },
+let books = [
+  { title: 'Uno Lorem',
+    author: 'Testero Testy',
+  },
+  { title: 'Dos Lorem',
+    author: 'Testero Testy2',
+  },
 ];
 
-function addBook() {
-    let newBook = new Book();
-    newBook.title = 'Tres Ipsum';
-    newBook.author = 'Testero Testy';
-    books.push(newBook);
-    console.log(books);
-};
-
-
-for (let i =0; i < books.length; i++) {
+function displayBooks() {
+document.getElementById('book-list').innerHTML = '';
+for (let i = 0; i < books.length; i++) {
   document.getElementById('book-list').innerHTML +=`
   <li class="book-card">
     <p>${books[i].title}</p>
     <p>${books[i].author}</p>
-    <button>Remove</button>
+    <button id="remove${i}"class="remove" onclick="removeBook(this.id)">Remove</button>
   </li>`;
 };
+};
 
+function addBook() {
+let newBook = {};
+newBook.title = document.getElementById('title').value;
+newBook.author = document.getElementById('author').value;
+if (newBook.title !='' && newBook.author != ''){
+books.push(newBook);
+document.getElementById('title').value = '';
+document.getElementById('author').value = '';
+catchValue();
+displayBooks();
+};
+};
+
+function removeBook(id) {
+let bookDeleted = id.replace('remove','');
+books.splice(bookDeleted,1);
+catchValue();
+displayBooks();
+};
+
+function catchValue() {
+const saveCollection = JSON.stringify(books);
+localStorage.setItem('books', saveCollection);
+};
+
+function getValue() {
+if (localStorage.getItem('books')) {
+  books = JSON.parse(localStorage.getItem('books'));
+  catchValue();
+}
+};
+
+getValue();
+window.onload = getValue;
+displayBooks();
